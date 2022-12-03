3aa68de672ea5feee5d30a8a0958621e48cfaf13
alice29shevchuk <95874389+alice29shevchuk@users.noreply.github.com>
2022-12-03 14:01:42 +0200

    add main window

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Assassin_sCreedWikiSked
 {
@@ -20,9 +21,20 @@ namespace Assassin_sCreedWikiSked
     /// </summary>
     public partial class MainWindow : Window
     {
+        public DispatcherTimer timer { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Tick += Progressbar;
+            timer.Interval = new TimeSpan(0, 0, 0,0,60);
+            timer.Start();
+        }
+
+        private void Progressbar(object sender, EventArgs e)
+        {
+            if(this.bar.Value == 100){timer.Stop();}
+            else{this.bar.Value += 1;}
         }
     }
 }
