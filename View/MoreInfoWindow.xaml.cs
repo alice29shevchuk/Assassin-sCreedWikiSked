6a3e9d2a9aac6d07b6901eba6d0b85b86f27e45c
ilyashevchenko <ilyashevchenko@ILYASCHEVCH3FA7>
2022-12-04 18:06:34 +0200

    update moreinfowindow

--- View/MoreInfoWindow.xaml.cs
+++ View/MoreInfoWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Assassin_sCreedWikiSked.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace Assassin_sCreedWikiSked.View
         public MoreInfoWindow()
         {
             InitializeComponent();
+            this.DataContext = new MoreInfoViewModel();
         }
     }
 }
---
+++ ViewModel/MoreInfoViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Assassin_sCreedWikiSked.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assassin_sCreedWikiSked.ViewModel
+{
+    public class MoreInfoViewModel : INotifyCollectionChanged
+    {
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
+        private ObservableCollection<Serie> series;
+
+        public ObservableCollection<Serie> Series
+        {
+            get { return series; }
+            set { series = value; }
+        }
+        public MoreInfoViewModel()
+        {
+            Series = new ObservableCollection<Serie>(new ModelContext().Series);
+        }
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+    }
+}
