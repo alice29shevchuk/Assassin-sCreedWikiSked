e4f61ca398ce461ce9245a6369f250e6cfd4b55c
alice29shevchuk <95874389+alice29shevchuk@users.noreply.github.com>
2022-12-05 14:20:47 +0200

    add media player in morewindow

--- View/Controls/UserPlayer.xaml.cs
+++ View/Controls/UserPlayer.xaml.cs
@@ -1,4 +1,5 @@
-﻿using CefSharp;
+﻿using Assassin_sCreedWikiSked.ViewModel;
+using CefSharp;
 using CefSharp.Wpf;
 using System;
 using System.Collections.Generic;
@@ -33,61 +34,23 @@ namespace Assassin_sCreedWikiSked.View.Controls
         public UserPlayer()
         {
             InitializeComponent();
-            Play();
+            PlayVideo();
         }
 
-        public async void Play()
+        public void PlayVideo()
         {
+            //MenuWindowViewModel menuWindowViewModel = new MenuWindowViewModel();
+            //MessageBox.Show(menuWindowViewModel.Series[0].VideoPath);
+            
             Match match = YouTubeURELIDRegex.Match("https://www.youtube.com/watch?v=eSjCeWcCtjY");
             String id = match.Groups["v"].Value;
             string url1 = "https://www.youtube.com/embed/" + id;
             string page = "<html>" +
                 "<head><meta http-equiv='X-UA-Compatible' content='IE=11'/>" +
-                "<body>" + "\r\n" +
-                "<iframe src=\"" + url1 + "\" width=\"260\" height=\"115\" frameborder=\"0\" allowfullscreen></iframe>" +
+                "<body style=\"background: #1e1f26\">" + "\r\n" +
+                "<iframe src=\"" + url1 + "\" width=\"950\" height=\"250\" frameborder=\"0\" allowfullscreen></iframe>" +
                 "</body></html>";
             this.web.NavigateToString(page);
-
-
-            //this.media = new MediaElement();
-            //this.media.Source = new Uri("https://www.youtube.com/embed/v2fDTOdWuQQ?rel=0&amp;showinfo=0",UriKind.RelativeOrAbsolute);
-            //this.media.LoadedBehavior = MediaState.Manual;
-            //this.media.Play();
-
-            //this.webBrowser = new ChromiumWebBrowser();
-            //CefSettings settings = new CefSettings();
-            //Cef.Initialize(settings);
-            //this.webBrowser = new ChromiumWebBrowser("https://www.youtube.com/embed/v2fDTOdWuQQ?rel=0&amp;showinfo=0");
-            ////this.list.Items.Add(this.webBrowser);
-            //this.media = new MediaElement();
-            //HttpClient http = new HttpClient();
-            //Uri uri = new Uri("https://www.youtube.com/embed/v2fDTOdWuQQ?rel=0&amp;showinfo=0");
-            //HttpRandomAccessStream
-            //this.web = new WebBrowser();
-            //this.web.Navigate(new Uri("https://youtu.be/_xkCPNECud8"));
-            //this.web.InvokeScript()
-
-            //this.player.Source.
-            ////var client = new YoutubeClient();
-            ////var streamManifest = await client.Videos.Streams.GetManifestAsync("https://youtu.be/_xkCPNECud8");
-            ////var streamInfo = (MuxedStreamInfo)streamManifest.GetMuxedStreams().GetWithHighestVideoQuality();
-
-            //this.media = new MediaElement();
-            ////this.media.Source = new Uri(@"https://dl8.webmfiles.org/big-buck-bunny_trailer.webm");
-            //this.media.Source = new Uri("https://youtu.be/_xkCPNECud8");
-            //this.media.Stretch = Stretch.Fill;
-            //this.media.Volume = 100;
-            //this.media.Visibility = Visibility.Collapsed;
-            //this.media.UnloadedBehavior = MediaState.Manual;
-            //this.media.Play();
-            ////this.media.Source = new Uri(streamInfo.Url,UriKind.RelativeOrAbsolute);
-            ////this.media = new MediaElement();
-            ////this.media.Source = new Uri(@"https://youtu.be/_xkCPNECud8", UriKind.RelativeOrAbsolute);
-            ////this.media.LoadedBehavior = MediaState.Manual;
-            ////this.media.Play();
-            //////this.media.Play();
-
-
         }
     }
 }
