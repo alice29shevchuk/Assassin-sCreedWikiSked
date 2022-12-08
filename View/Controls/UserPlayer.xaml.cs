4e4ed62d319f0646cb1e374bfab177fa4de8446b
ilyashevchenko <ilyashevchenko@ILYASCHEVCH3FA7>
2022-12-08 10:46:21 +0200

    v1.1

--- View/Controls/UserPlayer.xaml.cs
+++ View/Controls/UserPlayer.xaml.cs
@@ -42,15 +42,23 @@ namespace Assassin_sCreedWikiSked.View.Controls
         {
 
             MenuWindowViewModel menuWindowViewModel = new MenuWindowViewModel("series");
-            Match match = YouTubeURELIDRegex.Match(menuWindowViewModel.Series[Convert.ToInt32(ID) - 1].VideoPath);
-            String id = match.Groups["v"].Value;
-            string url1 = "https://www.youtube.com/embed/" + id;
-            string page = "<html>" +
-                "<head><meta http-equiv='X-UA-Compatible' content='IE=11'/>" +
-                "<body style=\"background: #1e1f26\">" + "\r\n" +
-                "<iframe src=\"" + url1 + "\" width=\"950\" height=\"250\" frameborder=\"0\" allowfullscreen></iframe>" +
-                "</body></html>";
-            this.web.NavigateToString(page);
+            try
+            {
+                Match match = YouTubeURELIDRegex.Match(menuWindowViewModel.Series[Convert.ToInt32(ID) - 1].VideoPath);
+                String id = match.Groups["v"].Value;
+                string url1 = "https://www.youtube.com/embed/" + id;
+                string page = "<html>" +
+                    "<head><meta http-equiv='X-UA-Compatible' content='IE=11'/>" +
+                    "<body style=\"background: #1e1f26\">" + "\r\n" +
+                    "<iframe src=\"" + url1 + "\" width=\"950\" height=\"250\" frameborder=\"0\" allowfullscreen></iframe>" +
+                    "</body></html>";
+                this.web.NavigateToString(page);
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+           
         }
     }
 }
